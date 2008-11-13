2f69c8ba7fafa3b1492ec1042a7c9c317f8a1f79
Jared Grippe <jared@deadlyicon.com>
2008-11-13 13:28:56 +0100

    finished coming soon page

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '42c174aa12f878df1a677ab369da10e3'
+  protect_from_forgery :secret => '4232213c174aa12ff1a677ab369da10e3'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
