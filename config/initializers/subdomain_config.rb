ea8e68ecb128c068ec4442506a60fcb18e6fd669
Jared Grippe <jared@deadlyicon.com>
2008-11-17 16:15:31 +0100

    added subdomain suppport and feeds controller

---
+++ config/initializers/subdomain_config.rb
@@ -0,0 +1,5 @@
+SubdomainFu.tld_sizes = {:development => 2,
+                         :test => 0,
+                         :production => 1}
+SubdomainFu.mirrors = ["www"]
+SubdomainFu.preferred_mirror = "www"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 ActionController::Routing::Routes.draw do |map|
+
+  # feeds.deadlyicon.com
+  map.with_options :conditions => { :subdomain => 'feeds' } do |feeds|
+    feeds.resources  :feeds,      :controller => 'feeds'
+    feeds.connect    '/:action/', :controller => 'feeds'
+    feeds.feeds_root '',          :controller => 'feeds',   :action => 'index'
+  end
+  
+  # www.deadlyicon.com
+  # deadlyicon.com
   map.resume 'resume', :controller => "resume", :action => "index"
   map.formatted_resume 'resume.:format', :controller => "resume", :action => "index"
   map.root :controller => "index", :action => "index"
