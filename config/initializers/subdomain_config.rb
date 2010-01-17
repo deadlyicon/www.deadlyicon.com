0db9b742873226bf23de3182a0ed5b28944c6717
Jared Grippe <jared@deadlyicon.com>
2010-01-16 20:58:23 -0800

    remove awesome and feeds and removed mysql dependency in production

--- config/initializers/subdomain_config.rb
+++
@@ -1,5 +0,0 @@
-SubdomainFu.tld_sizes = {:development => 2,
-                         :test => 0,
-                         :production => 1}
-SubdomainFu.mirrors = ["www"]
-SubdomainFu.preferred_mirror = "www"
